5ab1e8535be5223737ba28b6c21eb528d7e4f2fd
atlas0904 <u920243@gmail.com>
2017-03-31 16:03:27 +0800

    Upload ABS envioroment setup

---
+++ app/src/main/java/com/example/atlas_huang/libraryincludetest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.atlas_huang.libraryincludetest;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class MainActivity extends SherlockFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle(getApplication().getPackageName());
+    }
+}
---
+++ app/src/main/java/com/example/atlas_huang/libraryincludetest/MainFragment.java
@@ -0,0 +1,14 @@
+package com.example.atlas_huang.libraryincludetest;
+
+
+import android.support.v4.app.Fragment;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+/**
+ * Created by atlas_huang on 2017/3/31.
+ */
+
+public class MainFragment extends SherlockFragment {
+
+}
